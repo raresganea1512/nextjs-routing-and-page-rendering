40cbf954c9636a5a0462796847d86f68e17a9fd9
raresganea1512 <rganea67@gmail.com>
2024-07-03 21:54:00 +0300

    added dummy data, parallel routing and displaying it on the /archive path

--- app/archive/layout.js
+++ app/archive/layout.js
@@ -1,7 +1,9 @@
-export default function ArchiveLayout({archive, latest}){
-  return <div>
-    <h1>New Archives</h1>
-    <section id="archive-filter">{archive}</section>
-    <section id="archive-latest">{latest}</section>
-  </div>
+export default function ArchiveLayout({ archive, latest }) {
+  return (
+    <div>
+      <h1>News Archive</h1>
+      <section id="archive-filter">{archive}</section>
+      <section id="archive-latest">{latest}</section>
+    </div>
+  );
 }
